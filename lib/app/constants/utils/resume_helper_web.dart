c8a7a163545fc7926f0e0a34bc2d51500820ca8b
developer_1 <yadav.nilam.shardaprasad@brainiuminfotech.com>
2025-04-15 16:18:56 +0530

    Resume path updated

--- lib/app/constants/utils/resume_helper_web.dart
+++ lib/app/constants/utils/resume_helper_web.dart
@@ -1,7 +1,7 @@
 import 'dart:html' as html;
 
 void downloadWeb() {
-  final anchor = html.AnchorElement(href: 'assets/resume.pdf')
+  final anchor = html.AnchorElement(href: 'assets/resume/resume.pdf')
     ..setAttribute('download', 'Abrar_Khira_Resume.pdf')
     ..target = 'blank'
     ..click();
